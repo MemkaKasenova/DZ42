b2af49d49362b3ed0bb57d23310a7e2cfc3cebe3
Memka <mkasenova87@gmail.com>
2023-04-14 18:59:54 +0600

    Initial Commit

---
+++ DZ42/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ42
+//
+//  Created by merim kasenova on 14/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
